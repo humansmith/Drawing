cc9d899f603eafbc1adfa49b8b5cf56912d2d02e
Alister Smith <alister.psmith@gmail.com>
2023-04-14 16:26:00 +0200

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Alister on 14/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
